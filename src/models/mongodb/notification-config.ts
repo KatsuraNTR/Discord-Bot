9c5f404280223563b421465bb71fff19db8ca76b
KatsuraNTR <guanyi020718@outlook.com>
2024-04-21 16:36:02 +1000

    feat: Add MongoDB model for Youtube notifications

---
+++ src/models/mongodb/notification-config.ts
@@ -0,0 +1,73 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface INotificationConfig {
+    guildId: string;
+    notificationChannelId: string;
+    youtubeChannelId: string;
+    latestVideo?: {
+        snippet: {
+            url: string;
+            publishedAt: string;
+        };
+    };
+    scheduledVideos?: {
+        items: {
+            snippet: {
+                url: string;
+                publishedAt: string;
+                scheduledStartTime: string;
+            };
+        }[];
+    };
+}
+
+interface INotificationConfigModel extends INotificationConfig, Document {}
+
+const notificationConfigSchema = new Schema<INotificationConfig>(
+    {
+        guildId: {
+            type: String,
+            required: true,
+        },
+        notificationChannelId: {
+            type: String,
+            required: true,
+        },
+        youtubeChannelId: {
+            type: String,
+            required: true,
+        },
+        latestVideo: {
+            snippet: {
+                url: {
+                    type: String,
+                },
+                publishedAt: {
+                    type: String,
+                },
+            }
+        },
+        scheduledVideos: {
+            items: [
+                {
+                    snippet: {
+                        url: {
+                            type: String,
+                        },
+                        publishedAt: {
+                            type: String,
+                        },
+                        scheduledStartTime: {
+                            type: String,
+                        },
+                    },
+                },
+            ]
+        },
+    }
+);
+
+export const NotificationConfig = mongoose.model<INotificationConfigModel>(
+    'NotificationConfig',
+    notificationConfigSchema
+);
